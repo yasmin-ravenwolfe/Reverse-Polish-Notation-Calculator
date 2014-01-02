c7e3e95e5a7fa86f2d76f2e8e4b951caffcbdced
Yasmin Fazelinia <yfazelinia@gmail.com>
2014-01-02 12:09:44 -0600

    Remove @one_liner and @stack from Classifyer

--- lib/classifyer.rb
+++ lib/classifyer.rb
@@ -4,17 +4,19 @@ require_relative 'calculator'
 # After classification, a new instance of the appropriate class will be created.
 # The input-based class is responsible for knowing how to do a RPN calculation for that specific input type.
 # This class only knows about classifying input types.
+# 
+# This class acts as a Factory class- it is responsible for creating different types of objects.
+# 
 module RPN
   class Classifyer
-    # attr_accessor :one_liner, :stack
     def initialize
     end
 
     def classify
       if one_liner? == true
-        @one_liner = OneLiner.new
+        OneLiner.new
       else
-        @stack = Stack.new
+        Stack.new
       end
     end
 
